221c0f41de7f36d7f6b051333e5fc575106ed2cc
karu samuel ngugi <karusamuelngugi@gmail.com>
2022-03-06 12:09:49 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
+let num1 = 31;
+let num2 = 2;
+let set = (20,2,2,3,4,1);
 // Write your code here
+let multiply = function(num1,num2){
+
+  return num1*num2;
+}
+
+let random = Math.floor(Math.random()*10)+1;
+
+
+let mod = function(num3,num4){
+    return num3 % num4;
+}
+
+let max = function(set){
+    return Math.max(set);
+}
